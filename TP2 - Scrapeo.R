293830f5fa9e51c58f48a5d39cb7c99ae7475065
Georgina Gorlero <georgina.gorlero@gmail.com>
2021-08-16 19:58:55 -0300

    Armado del proyecto y el RScript. Carga de las librerías

---
+++ TP2 - Scrapeo.R
@@ -0,0 +1,10 @@
+
+#Trabajo Práctico n° 2 - Scrapeo
+
+#Comenzamos trayendo las librerías que vamos a necesitar
+
+#install.packages("rvest")
+library(rvest)
+library(tidyverse)
+
+read_html("https://es.wikipedia.org/wiki/Vacunaci%C3%B3n_contra_la_COVID-19_en_Argentina")
