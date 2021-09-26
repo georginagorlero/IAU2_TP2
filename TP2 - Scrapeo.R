35e5fc3d00fe6aaf5c4a61d83904501c50044656
Georgina Gorlero <georgina.gorlero@gmail.com>
2021-09-26 18:30:21 -0300

    cambios realizados por modificaciones en la web de wikipedia que scrapee

--- TP2 - Scrapeo.R
+++ TP2 - Scrapeo.R
@@ -8,6 +8,7 @@ library(rvest) # Easily Harvest (Scrape) Web Pages
 library(tidyverse) # Easily Install and Load the 'Tidyverse' 
 library(janitor) # Simple Tools for Examining and Cleaning Dirty Data
 library(lubridate) # Make Dealing with Dates a Little Easier
+library(stringr)
 
 #Vamos a trabajar con la información de la llegada de las vacunas Covid-19 a la Argentina
 wikipedia_url <- "https://es.wikipedia.org/wiki/Vacunaci%C3%B3n_contra_la_COVID-19_en_Argentina"
@@ -17,14 +18,14 @@ vacunascovid_arg <- read_html(wikipedia_url)
 #Probamos primero trayendo la tabla a través de html_elements()
 tabla <- vacunascovid_arg %>% 
   html_elements(".wikitable") %>%  #Nos trae una lista con las 12 tablas de la URL.
-  `[[`(6) %>%  #Fuimos viendo tabla por tabla hasta saber que la que tenía que traer era la 6
+  `[[`(7) %>%  #Fuimos viendo tabla por tabla hasta saber que la que tenía que traer era la 7
   html_table()
 
 tabla
 
 #Probamos con Xpath
 tabla2 <- vacunascovid_arg %>% 
-  html_nodes(xpath='//*[@id="mw-content-text"]/div[1]/table[10]') %>%
+  html_nodes(xpath='//*[@id="mw-content-text"]/div[1]/table[11]') %>%
   html_table(fill=T)
 
 tabla2
@@ -48,11 +49,31 @@ info_header <- as.vector(tabla2[1,], mode="character")
  
 tabla3 <- tabla2[3:78,1:5]%>% #Nos quedamos con la parte de la tabla que necesitamos
           `colnames<-`(header)%>% #y le cambiamos el nombre a las columnas por los correctos
-          clean_names()%>% #Limpiamos los nombres de las columnas con janitor
-          mutate(fecha_de_llegada=dmy(tabla3$fecha_de_llegada))%>% #modificamos la columna de fechas a una con class=date
-          mutate(vacuna=as.factor(tabla3$vacuna))%>% #modificamos la columna vacuna y laboratorio por factores
-          mutate(laboratorio=as.factor(tabla3$laboratorio)) 
-tabla3
+          clean_names() #Limpiamos los nombres de las columnas con janitor
 
+tabla3$fecha_de_llegada <- str_replace_all(tabla3$fecha_de_llegada, "de","")%>% #para pasar las fechas a inglés tal que el lubridate las reconozca vamos a primero sacar los "de"
+                           str_replace_all(c("enero" = "january",               #pasamos cada mes a su equivalente en inglés  
+                                             "febrero" = "february",
+                                             "marzo" ="march",
+                                             "abril"="april", 
+                                             "mayo"="may", 
+                                             "junio"="june", 
+                                             "julio"="july",
+                                             "agosto"="august",
+                                             "septiembre"="september",
+                                             "octubre" = "october",
+                                             "noviembre" ="november",
+                                             "diciembre" = "december"))
+                          
+tabla3$cantidad_de_dosis <- gsub("[[:space:]]", "", tabla3$cantidad_de_dosis)%>%
+                            as.integer()
+
+tabla3 <- tabla3 %>%
+          mutate(fecha_de_llegada=dmy(tabla3$fecha_de_llegada)) %>% #modificamos la columna de fechas a una con class=date
+          mutate(vacuna=as.factor(tabla3$vacuna)) %>% #modificamos la columna vacuna y laboratorio por factores
+          mutate(laboratorio=as.factor(tabla3$laboratorio))
+
+
+tabla3
 
 
